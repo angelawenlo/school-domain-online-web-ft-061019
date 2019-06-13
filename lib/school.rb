96c816d178e05a3112050778c1e0ff766f8fb713
Angela Lo <angelawenlo@gmail.com>
2019-06-13 16:16:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+require 'pry'
+class School
+  attr_reader :roster, :school_name
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  def add_student(name, grade)
+    @roster[grade] = []
+    if @roster.include?(grade)
+      @roster
+    else
+      @roster[grade] << name
+    end
+  end
+  def grade(grade)
+    @roster[grade]
+  end
+end
